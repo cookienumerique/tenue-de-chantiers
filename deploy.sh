06f90dfd20895ce200a8d498f34915fa577999c6
cookienumerique <cookienumerique@gmail.com>
2023-09-14 14:53:50 +0200

    Maj script pm2

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/www/cookie-numerique || exit;
 git pull origin master;
-yarn install && yarn build && pm2 restart cookie-numerique
+yarn install && yarn build && pm2 restart cookie_numerique
