6449eae6d89498dcf6328954c9d65a0f389a63a2
cookienumerique <cookienumerique@gmail.com>
2023-09-18 16:47:48 +0200

    deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd ~/var/www/cookie-numerique || exit;
 git pull origin main;
+rm -rf nodes_modules
+rm -f package-lock.json;
+rm -f yarn-lock.json
+npm cache clear --force;
 npm install --loglevel verbose;
 npm run build --verbose;
 pm2 restart cookie_numerique
