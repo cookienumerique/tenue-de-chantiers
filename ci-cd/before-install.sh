6ecfea95618f3505199a4fca9e4341140c2da898
Cavelier Mickael <cavelier.mickael@cookie-numerique.fr>
2023-09-18 22:22:50 +0200

    test deploy

--- ci-cd/before-install.sh
+++ ci-cd/before-install.sh
@@ -1,6 +1,7 @@
 cd /var/www/cookie-numerique || exit;
 git pull origin main;
 rm -rf /var/www/cookie-numerique/node_modules;
+sudo apt install nodejs npm;
 sudo npm install -g yarn;
 yarn cache clean;
 
