07794685995267e15cf43d4e080af8bf1e7e1f20
cookienumerique <cookienumerique@gmail.com>
2023-09-18 16:39:06 +0200

    deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd ~/var/www/cookie-numerique || exit;
 git pull origin main;
-npm install && npm run build && pm2 restart cookie_numerique
+npm install --loglevel verbose;
+npm run build --verbose;
+pm2 restart cookie_numerique
