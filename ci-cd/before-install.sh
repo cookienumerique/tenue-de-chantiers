c12100b8c7b5d3342a5bbbbb31a700337a5c107e
cookienumerique <cookienumerique@gmail.com>
2023-09-19 11:56:50 +0200

    deploy sh

--- ci-cd/before-install.sh
+++ ci-cd/before-install.sh
@@ -1,6 +1,6 @@
 cd /var/www/cookie-numerique || exit;
 git pull origin main;
-rm -rf /var/www/cookie-numerique/node_modules;
+rm -rf node_modules;
 sudo apt install -y nodejs npm;
 sudo npm install -g yarn;
 yarn cache clean;
