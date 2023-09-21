ff34ae797df98981a4cac1229ba6e4400d824f5a
Cavelier Mickael <cavelier.mickael@cookie-numerique.fr>
2023-09-21 19:23:16 +0200

    test deploy

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 cd /var/www/cookie-numerique || exit
-npm start
-pm2 start npm --name "cookie_numerique" -- start
-pm2 startup
-pm2 save
-pm2 restart all
+npm install
+npm install pm2 -g
--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /var/www/cookie-numerique || exit
-npm install
-npm install pm2 -g
+npm start
+pm2 start npm --name "cookie_numerique" -- start
+pm2 startup
+pm2 save
+pm2 restart all
