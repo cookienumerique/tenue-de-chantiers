9cabcbb920f368e2ef91c60acecd36d9d3b93b76
Cavelier <cookienumerique@gmail.com>
2024-01-10 13:59:03 +0100

    Build(package): Modifier le script de deploiement

--- deploiement/deploy-production.sh
+++ deploiement/deploy-production.sh
@@ -1,6 +1,6 @@
-rm -rf nodes_modules;
-npm install --production;
-
+rm -rf node_modules;
+rm -rf .next;
+npm install --max-old-space-size=4096;
 npm run build;
 #pm2 start npm --name "tenue-de-chantiers" -- start;
 pm2 restart tenue-de-chantiers
--- deploy-production.sh
+++
@@ -1,3 +0,0 @@
-rm -rf node_modules
-npm install --max-old-space-size=4096
-pm2 restart tenue-de-chantiers
